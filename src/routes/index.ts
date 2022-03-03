376a8c238d418f70cb388d01a514fe9af62c417a
Arthur <andradearthurf@gmail.com>
2022-03-03 20:11:49 -0300

    user entity

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
@@ -9,5 +10,7 @@ router.use("/categories", categoriesRoutes); // Usando a rota criada em routes.
 // Com isso, todos os métodos POST, GET, etc, serão passados para o nosso server.
 
 router.use("/specifications", specificationsRoutes);
+
+router.use("/users", usersRoutes);
 
 export { router };
